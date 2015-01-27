c606cb045a35bad6a7f5387e6d5048e29e7c1b95
Felipe Forbeck <fforbeck@gmail.com>
2015-01-26 22:48:44 -0200

    including scatter chart

--- app/actors/TweetSupervisor.java
+++ app/actors/TweetSupervisor.java
@@ -47,11 +47,11 @@ public final class TweetSupervisor extends UntypedActor {
     public void onReceive(Object objMsg) throws Exception {
         if (objMsg instanceof Start) {
             Start message = (Start) objMsg;
-            startSimpleTweetHarvester(message.getHashTag(), message.getLang());
-            startRealtimeTweetReceiver(message.getHashTag(), message.getLang());
+           // startSimpleTweetHarvester(message.getHashTag(), message.getLang());
+           // startRealtimeTweetReceiver(message.getHashTag(), message.getLang());
 
         } else if (objMsg instanceof Read) {
-            startTweetAnalyzer(((Read) objMsg).getHashTag());
+            // startTweetAnalyzer(((Read) objMsg).getHashTag());
         } else {
             unhandled(objMsg);
         }
